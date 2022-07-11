34c7a8a97f2eb87206632b8cc3f7c204c363c5d6
Priyansh Sharma <priyansh_sharma_15@outlook.com>
2022-07-11 18:22:53 +0530

    Ex1

---
+++ ex1/ex1.rb
@@ -0,0 +1,29 @@
+require 'selenium-webdriver'
+
+class LeetCode
+  def initialize(driver)
+    @driver=driver
+  end
+
+  def l_home(search)
+    @driver.find_elements(:tag_name, 'h3').each do |element|
+      if element.text.eql?(search)
+        element.click
+        break
+      end
+    end
+  end
+
+  def navigate(search)
+    @driver.navigate.to 'https://www.google.com/'
+    a=@driver.find_element(name: 'q')
+    a.send_keys(search)
+    a.send_keys(:enter)
+    sleep 5
+    l_home('LeetCode - The World\'s Leading Online Programming ...')
+    sleep 20
+  end
+end
+
+leet = LeetCode.new(Selenium::WebDriver.for :chrome)
+leet.navigate('leetcode')
